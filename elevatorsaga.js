138587b282247df1ae13dc7299b84ed4e19a10a6
John Krah <john.krah@socrata.com>
2015-01-22 19:49:59 -0800

    so much fun

---
+++ elevatorsaga.js
@@ -0,0 +1,109 @@
+{
+    init: function(elevators, floors) {
+        var fbot = floors[0].floorNum(), ftop = floors[floors.length-1].floorNum();
+        var fhalf = floors[Math.floor(floors.length/2)].floorNum();
+        var uppers = [], downers = [];
+        
+        var move = function(ei, n) {
+              var elev = elevators[ei];
+              var c = elev.currentFloor();
+              var up = (c === 0 || c < n);
+              var d = up ? "up" : "down";
+              console.log("move elevator " + ei + " " + d + " from " + c + " to " + n);
+              elev.goingDownIndicator(!up);
+              elev.goingUpIndicator(up);
+              elev.goToFloor(n);
+        }
+        
+        var carString = function(ei) {
+            var elev = elevators[ei];
+            var d = elev.goingDownIndicator() ? "D" : " ", u = elev.goingUpIndicator() ? "U" : " ";
+            var l = elev.loadFactor();
+            return "car " + ei + " [" + d + u + "] {" + l + "}";
+        }
+        
+        _.each(floors, function(floor) {
+          var n = floor.floorNum();
+          floors[n].on("up_button_pressed", function() {
+            if (uppers.indexOf(n) === -1) {
+              uppers.push(n)
+              uppers.sort();
+            }
+            console.log("uppers + " + n + " = " + uppers.join());
+          });
+          floors[n].on("down_button_pressed", function() {
+            if (downers.indexOf(n) === -1) {
+              downers.push(n);
+              downers.sort().reverse();
+            }
+            console.log("downers + " + n + " = " + downers.join());
+          });
+        });
+        
+        _.each(elevators, function(elev) {
+            var ei = elevators.indexOf(elev), goingup = true, callup = true, callfn = -1;
+            elev.on("idle", function() {
+                setTimeout(5000);
+                console.log(carString(ei) + " idle");
+                var n = -1;
+                if (uppers.length !== 0) {
+                    n = uppers.shift();
+                    callfn = n;
+                    callup = true;
+                } else if (downers.length !== 0) {
+                    n = downers.shift();
+                    callfn = n;
+                    callup = false;
+                }
+                
+                if (n > -1) {
+                    move(ei, n);
+                } else {
+                    move(ei, 0);
+                }
+            });
+            elev.on("floor_button_pressed", function(n) {
+                move(ei, n);
+                elev.destinationQueue.sort();
+                if (elev.goingDownIndicator()) {elev.destinationQueue.reverse(); }
+                //console.log(" " + ei + " request floor + " + n + " = " + elev.destinationQueue.join());
+            });
+            elev.on("passing_floor", function(n, d) {
+                goingup = d === "up";
+                var stops = downers;
+                if (goingup) { stops = uppers; }
+                console.log(carString(ei) + " past floor " + n + ". " + "stops " + stops.join());
+                if (elev.loadFactor() < 0.99) {
+                    var i = stops.indexOf(n);
+                    if (i > -1) {
+                      elev.goToFloor(n, true);
+                      if (goingup) { uppers.splice(i,1); }
+                      else { downers.splice(i,1); }
+                    }
+                }
+            });
+            elev.on("stopped_at_floor", function(n) {
+                console.log(carString(ei) + " stop on floor " + n);
+                elev.goingDownIndicator(!goingup);
+                elev.goingUpIndicator(goingup);
+                if (n === fbot || n === ftop || elev.destinationQueue === []) {
+                    elev.goingDownIndicator(true);
+                    elev.goingUpIndicator(true);
+                } else if (n == callfn) {
+                    elev.goingDownIndicator(!callup);
+                    elev.goingUpIndicator(callup);
+                } else if (floors[n].buttonStates.down !== "") {
+                    elev.goingDownIndicator(true);
+                } else if (floors[n].buttonStates.up !== "") {
+                    elev.goingUpIndicator(true);
+                };
+            });
+        });
+    },
+    update: function(dt, elevators, floors) {
+        _.each(floors, function(floor) {
+            if (floor.buttonStates.down !== "") { floor.pressDownButton(); }
+            if (floor.buttonStates.up !== "") { floor.pressUpButton(); }
+        });
+    }
+}
